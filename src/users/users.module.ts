b24e9ddd277f0c077d92662baf11544e6f7befe4
nguyenthuong <nguyen.ngoc.thuong@sun-asterisk.com>
2022-03-14 08:47:59 +0700

    user subcriber

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+
+import { UsersService } from './users.service';
 import { PostsService } from 'src/posts/posts.service';
+import { UsersResolver } from './users.resolver';
+import { User } from './entities/user.entity';
 import { Post } from 'src/posts/entities/post.entity';
+import { UserSubscriber } from './entities/user.subcriber';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Post])],
-  providers: [UsersResolver, UsersService, PostsService]
+  providers: [UsersResolver, UsersService, PostsService, UserSubscriber]
 })
 export class UsersModule { }
